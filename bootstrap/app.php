7a1ab8cd908cc354e6f290f2a885b708a3ffa234
Mehrdad Saami <MehrdadSaami@gmail.com>
2019-07-30 00:51:39 +0430

    add env file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,3 +7,13 @@ session_start();
 // include autoloader 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
+
+try{
+	// load env file
+	$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+	$dotenv->load();
+} catch(Dotenv\Exception\InvalidPathException $e){
+	die('Please create .env file');
+}
+
